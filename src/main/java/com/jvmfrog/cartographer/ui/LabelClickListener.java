c90279860b1288c9e160d82d1dd6278eb9aa25d0
KirboGames <kirbo06@mail.ru>
2022-12-12 20:47:36 +0100

    кароче мне лень переводите сами
    - добавил создание меток
    - добавил сохранение координат в метку
    - следующий коммит думаю будет пре-релизом

--- src/main/java/com/jvmfrog/cartographer/ui/LabelClickListener.java
+++ src/main/java/com/jvmfrog/cartographer/ui/LabelClickListener.java
@@ -1,5 +1,7 @@
 package com.jvmfrog.cartographer.ui;
 
+import de.tr7zw.nbtapi.NBTCompound;
+import de.tr7zw.nbtapi.NBTItem;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -13,14 +15,14 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 public class LabelClickListener implements Listener {
     @EventHandler
-    public void eventRightClick(PlayerInteractEvent event) {
+    public void labelRightClick(PlayerInteractEvent event) {
         if (event.getItem() == null) return;
         if (!event.getItem().getItemMeta().getDisplayName().equals("LABEL")) return;
         if (event.getItem().getType() != Material.PAPER) return;
         if (!event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
 
         event.getItem().add(-1);
-        ItemStack item = new ItemStack(Material.PAPER);
+        ItemStack item = new ItemStack(Material.MAP);
 
         Block block = event.getClickedBlock();
 
@@ -28,11 +30,58 @@ public class LabelClickListener implements Listener {
         int y = block.getY();
         int z = block.getZ();
 
-        ItemMeta meta = Bukkit.getItemFactory().getItemMeta(Material.PAPER);
-        meta.displayName(Component.text(String.format("X: %d, Y: %d, Z: %d", x, y, z)));
+
+        ItemMeta meta = Bukkit.getItemFactory().getItemMeta(Material.MAP);
+        meta.displayName(Component.text(String.format("Label X: %d, Y: %d, Z: %d", x, y, z)));
 
         item.setItemMeta(meta);
 
+        NBTItem nbt = new NBTItem(item);
+        NBTCompound cartographer = nbt.addCompound("cartographer");
+        cartographer.setInteger("x", x);
+        cartographer.setInteger("y", y);
+        cartographer.setInteger("z", z);
+        String dim = switch (block.getWorld().getEnvironment()) {
+            case NORMAL -> "overworld";
+            case NETHER -> "nether";
+            case THE_END -> "end";
+            default -> "";
+        };
+        cartographer.setString("dim", dim);
+        item = nbt.getItem();
+
         event.getPlayer().getInventory().addItem(item);
+    }
+
+    @EventHandler
+    public void mapCLick(PlayerInteractEvent event) {
+        if (event.getItem() == null) return;
+        if (event.getItem().getType() != Material.MAP) return;
+        NBTItem item = new NBTItem(event.getItem());
+        if (!item.hasKey("cartographer")) return;
+        event.setCancelled(true);
+
+        Action action = event.getAction();
+        String name = action == Action.LEFT_CLICK_BLOCK ?
+                "box_start" : action == Action.RIGHT_CLICK_BLOCK ? "box_end" : null;
+        System.out.println(name);
+        if (name != null)
+            event.getPlayer()
+                    .getInventory()
+                    .setItemInMainHand(saveCoords(event.getItem(), name, event.getClickedBlock()));
+    }
+
+    private ItemStack saveCoords(ItemStack item, String name, Block block) {
+        int x = block.getX();
+        int y = block.getY();
+        int z = block.getZ();
+        NBTItem nbt = new NBTItem(item);
+        NBTCompound cartographer = nbt.getCompound("cartographer");
+        NBTCompound coords = cartographer.addCompound(name);
+        coords.setInteger("x", x);
+        coords.setInteger("y", y);
+        coords.setInteger("z", z);
+
+        return nbt.getItem();
     }
 }
